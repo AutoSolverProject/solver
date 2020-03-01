427f62f11cf65750111851120f3ad16b73e7564c
Tomer Michael <tomer.michael7@gmaill.com>
2020-03-01 19:32:30 +0200

    add sat_solver.py

---
+++ sat_solver.py
@@ -0,0 +1,31 @@
+
+def tseitin_transformation():
+    pass
+
+
+def preprocess():
+    pass
+
+
+def deduction_steps():
+    pass
+
+
+def case_splitting():
+    pass
+
+
+def conflict_analysis():
+    pass
+
+
+def learn_conflict_clauses():
+    pass
+
+
+def decision_heuristic():
+    pass
+
+
+def watch_literals():
+    pass
