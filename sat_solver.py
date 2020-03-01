a697ca1d03e5a57edaaed8dacddf61b772eee5dd
Tomer Michael <tomer.michael7@gmaill.com>
2020-03-01 22:13:55 +0200

    tseitin unfinished

--- sat_solver.py
+++ sat_solver.py
@@ -1,5 +1,39 @@
 
+from propositional_logic.syntax import *
+from logic_utils import *
+
+
 def tseitin_transformation():
+    pass
+
+
+def find_all_sub_formulas(formula):
+
+    sub_formulas = set()
+
+    def find_all_sub_formulas_helper(sub_formula):
+        if is_variable(sub_formula.root) or is_constant(sub_formula.root):
+            return
+
+        elif is_unary(sub_formula.root):
+            sub_formulas.add(sub_formula)
+            find_all_sub_formulas(sub_formula.first)
+
+        elif is_binary(sub_formula.root):
+            sub_formulas.add(sub_formula)
+            find_all_sub_formulas(sub_formula.first)
+            find_all_sub_formulas(sub_formula.second)
+
+    return find_all_sub_formulas(formula)
+
+
+def give_representation_to_subformulas(formula):
+    all_sub_formulas = find_all_sub_formulas(formula)
+    representation = {sub_formula: next(fresh_variable_name_generator) for sub_formula in all_sub_formulas}
+    return representation
+
+
+def bind_represntation_to_subformulas(formula):
     pass
 
 
