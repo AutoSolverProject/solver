046c48879be48836a37c2daa272e1d40f71c4662
Tomer Michael <tmichael@lightricks.com>
2020-03-17 23:37:35 +0200

    Finish Pre-processing

--- cnfformula.py
+++ cnfformula.py
@@ -1,21 +1,74 @@
+import copy
 
 from logic_utils import frozen
 from typing import List
 
 
 @frozen
-class CNFClause:
+class CNFVariable:
 
-    def __init__(self, variables: List[str]):
-        self.variables = variables
+    def __init__(self, name: str, is_negated: bool):
+        self.name = name
+        self.is_negated = is_negated
 
 
     def __repr__(self) -> str:
-        return self.variables.__repr__()
+        return "~" + str(self.name) if self.is_negated else str(self.name)
+
+
+    def __lt__(self, other: object):
+        if not isinstance(other, CNFVariable):
+            return True
+        if self.name == other.name:
+            return self.is_negated and not other.is_negated
+        else:
+            return self.name < other.name
+
+
+    def __le__(self, other: object):
+        return self < other or self == other
 
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, CNFClause) and str(self) == str(other)
+        return isinstance(other, CNFVariable) and str(self) == str(other)
+
+
+    def __ne__(self, other: object) -> bool:
+        return not self == other
+
+
+    def __gt__(self, other):
+        return not self <= other
+
+
+    def __ge__(self, other):
+        return not self < other
+
+
+    def __hash__(self) -> int:
+        return hash(str(self))
+
+
+@frozen
+class CNFClause:
+
+    def __init__(self, literals: List[CNFVariable]):
+        self.literals = copy.deepcopy(literals)
+        self.literals.sort()
+
+        self.positive_literals = [literal for literal in literals if not literal.is_negated]
+        self.positive_literals.sort()
+
+        self.negative_literals = [literal for literal in literals if literal.is_negated]
+        self.negative_literals.sort()
+
+
+    def __repr__(self) -> str:
+        return self.literals.__repr__()
+
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, CNFClause) and self.literals == other.literals
 
 
     def __ne__(self, other: object) -> bool:
@@ -24,6 +77,10 @@ class CNFClause:
 
     def __hash__(self) -> int:
         return hash(str(self))
+
+
+    def __len__(self):
+        return len(self.literals)
 
 
 @frozen
@@ -41,7 +98,7 @@ class CNFFormula:
 
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, CNFFormula) and str(self) == str(other)
+        return isinstance(other, CNFFormula) and self.clauses == other.clauses
 
 
     def __ne__(self, other: object) -> bool:
@@ -50,3 +107,7 @@ class CNFFormula:
 
     def __hash__(self) -> int:
         return hash(str(self))
+
+
+    def __len__(self):
+        return len(self.clauses)
--- sat_solver.py
+++ sat_solver.py
@@ -5,8 +5,10 @@ from normal_forms import *
 from propositional_logic.syntax import *
 
 
-def tseitin_transformation(formula):
-    nnf_formula = to_nnf(formula)  # Simplifying assumption
+# region Tseitin transformation
+
+def tseitin_transformation(propositional_formula):
+    nnf_formula = to_nnf(propositional_formula)  # Simplifying assumption
     representations = give_representation_to_sub_formulae(nnf_formula)
 
     first_clause = CNFClause(representations[nnf_formula])
@@ -26,8 +28,8 @@ def tseitin_transformation(formula):
     return CNFFormula(clauses)
 
 
-def give_representation_to_sub_formulae(formula):
-    all_sub_formulae = find_closure(formula)
+def give_representation_to_sub_formulae(propositional_formula):
+    all_sub_formulae = find_closure(propositional_formula)
     rep_generator = __prefix_with_index_sequence_generator('pg')
     representation = dict()
 
@@ -36,30 +38,83 @@ def give_representation_to_sub_formulae(formula):
 
     return representation
 
+# endregion
 
-def preprocess():
-    pass
 
+# region Pre-processing
+
+def preprocess(cnf_formula):
+    new_clauses = list()
+
+    for clause in cnf_formula.clauses:
+        if is_trivial_clause(clause):
+            continue
+        new_clause = remove_redundant_literals(clause)
+        new_clauses.append(new_clause)
+
+    return CNFFormula(new_clauses)
+
+
+def remove_redundant_literals(cnf_clause):
+    return CNFClause(list(set(cnf_clause.literals)))
+
+
+def is_trivial_clause(cnf_clause):
+    positive_set = set(cnf_clause.positive_literals)
+    negative_set = set(cnf_clause.negative_literals)
+    intersection = positive_set & negative_set
+    return len(intersection) != 0
+
+# endregion
+
+
+# region Deduction steps
 
 def deduction_steps():
     pass
 
 
+def unit_propagation():
+    pass
+
+# endregion
+
+
+# region Case splitting
+
 def case_splitting():
     pass
 
+# endregion
+
+
+# region Conflict analysis
 
 def conflict_analysis():
     pass
 
+# endregion
+
+
+# region Learning conflict clauses
 
 def learn_conflict_clauses():
     pass
 
+# endregion
+
+
+# region Decision heuristics
 
 def decision_heuristic():
     pass
 
+# endregion
+
+
+# region Watch literals
 
 def watch_literals():
     pass
+
+# endregion
