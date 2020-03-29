70fcb61f01ff40c39f64eb16f53f72eba1d2b4a1
ofer.rvd <ofer.rvd@cs.huji.ac.il>
2020-03-29 23:37:03 +0300

    Add prints to main file

--- solver.py
+++ solver.py
@@ -37,11 +37,15 @@ def main():
     unsat_fo_formulae = {fo_formula3, fo_formula4, fo_formula5}
     print("Verify smt_solver by running it on Tuf formulae.\n\n")
     for formula in sat_fo_formulae:
+        print("Checking T-satisfiability of formula " + str(formula))
         state, model = smt_solver(formula)
         assert state == SAT
+        print("The formula " + str(formula) + ", is Tuf-satisfiable.")
     for formula in unsat_fo_formulae:
+        print("Checking T-satisfiability of formula " + str(formula))
         state, model = smt_solver(formula)
         assert state == UNSAT
+        print("The formula " + str(formula) + ", is not Tuf-satisfiable.")
 
 
 if __name__ == '__main__':
